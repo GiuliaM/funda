d79b2a34fa9493a278eae80cd613185a2642b428
GiuliaM <Giulia@211.221.028.145.hva.nl>
2017-03-08 10:33:52 +0100

    working filter on search

--- static/app.js
+++ static/app.js
@@ -26,13 +26,13 @@
             routie({
                 'home': function() {
                     sections.toggle(location.hash);
-                    console.log('Youre at the homepage');
+//                    console.log('Youre at the homepage');
                 },
 
                 'home/:id': function(id) {
                     sections.toggle(location.hash);
                     getData.details(id);
-                    console.log('Youre at the detailpage');
+//                    console.log('Youre at the detailpage');
                 }
             });
         }
@@ -40,20 +40,18 @@
 
     var getData = {
         //  With this function you tell to take the query from the input field and how to construct the new url.
-        overview: function() {
-            // call to api movie list
+        overview: function(userQuery) {
+            var self = this;
             var searchInput = document.getElementById('user-input-field').value;
-            var apiUrl = config.searchApi + apiKey + '/?type=koop&zo=/' + searchInput + '&page=1&pagesize=25';
-            console.log(apiUrl);
-            console.log(searchInput);
 
-            //aja is the mini library.  With .url you tell where to get the info.  With .on you say: if successfull load the data in function(data).
+            var apiUrl = config.searchApi + apiKey + '/?type=koop&zo=/' + searchInput + '&page=1&pagesize=25';
+
             aja()
                 .url(apiUrl)
                 .on('success', function(data) {
-
-                    console.log(data + "aja");
-                    sections.overview(data);
+//                    sections.overview(data);
+                    self.filter(data);
+//                    console.log(data);
                 })
             .go();
         },
@@ -66,13 +64,31 @@
             aja()
                 .url(detailUrl)
                 .on('success', function(data) {
-
-                    console.log(data, "You see me");
                     sections.details(data);
                 })
                 .go();
-        }
+        },
+//
+        filter: function(data){
+
+            function getKamers(kamers) {
+                return kamers.AantalKamers >= 5;
+            }
+
+            var filterdata = data.Objects.filter(getKamers);
+            console.log(filterdata);
+            return sections.overview(filterdata);
+            }
+
     };
+
+
+//            document.getElementById('queryResult').innerHTML = filterdata
+//                .map(function(info, i) {
+//                    return getData.filter(info, i);
+//                });
+//        }
+//    };
 
     var sections = {
         overview: function(data) {
@@ -81,12 +97,11 @@
             var html = '';
 
             // With this function you tell what you want to show when a query is requested.
-            data.Objects.map(function(element) {
-
+            data.map(function(element) {
+                    console.log(element);
             //snipet no foto
-                console.log(element);
 
-                html += '<div class="searchResult" id="' + element.Id + '"> <a href="#home/' + element.Id + '"><h1>' + element.Adres + '</h1> <img src= "' + element.FotoLarge + '"/> </div></a>';
+                html += '<div class="searchResult" id="' + element.Id + '"> <a href="#home/' + element.Id + '"><h1>' + element.Adres + '</h1><h1>' + element.AantalKamers + '</h1> <img src= "' + element.FotoLarge + '"/> </div></a>';
             });
 
             document.getElementById('queryResult').innerHTML = html;
@@ -98,15 +113,46 @@
 
             //snippet no foto
 
-            htmlDetail += '<div class="detailResult" id="' + detail.Id + '"><img src= "' + detail.HoofdFoto + '"/> <div class="detailBlok"> <h1>' + detail.Adres + '</h1> <h2>Omschrijving</h2><p>'+ detail.VolledigeOmschrijving +'</p> <h2>Budget</h2><p>'+ detail.Koopprijs +'</p> <a href="#movies"> Go back to overview</a> </div> </div>';
+            htmlDetail += '<div class="detailResult" id="' + detail.Id + '"><img src= "' + detail.HoofdFoto + '"/> <div class="detailBlok"> <h1>' + detail.Adres + '</h1> <h2>Omschrijving</h2><p>'+ detail.VolledigeOmschrijving +'</p> <h2>Koopprijs</h2><p>'+ detail.Koopprijs +'</p> <h2>Soort woning</h2><p>'+ detail.SoortWoning +'</p> <h2>Aantal kamers</h2><p>'+ detail.AantalKamers +'</p> <h2>Energielabel</h2><p>'+ detail.Energielabel.Label +'</p><a href="#home"> Go back to overview</a> </div> </div>';
 
             document.getElementById('showDetails').innerHTML = htmlDetail;
 
         },
 
+
+
+//        filter: function(filterdata) {
+//            //render html with data
+//            var htmlFilter = '';
+//
+//            //snippet no foto
+//
+//            filterDetail += '<div class="detailResult" id="' + detail.Id + '"><img src= "' + detail.HoofdFoto + '"/> <div class="detailBlok"> <h1>' + detail.Adres + '</h1> <h2>Omschrijving</h2><p>'+ detail.VolledigeOmschrijving +'</p> <h2>Koopprijs</h2><p>'+ detail.Koopprijs +'</p> <h2>Soort woning</h2><p>'+ detail.SoortWoning +'</p> <h2>Aantal kamers</h2><p>'+ detail.AantalKamers +'</p><a href="#movies"> Go back to overview</a> </div> </div>';
+//
+//            document.getElementById('showDetails').innerHTML = htmlFilter;
+//
+//
+//            function(data) {
+////                function getKamers(kamers) {
+////                    return kamers.AantalKamers >= 7;
+////                console.log(kamers);
+////            }
+////
+////            var filterdata = data.Objects.filter(getKamers);
+////            document.getElementById('overview').innerHTML = filterdata
+////                .map(function(info, i) {
+////                    return sections.filter(info, i);
+////                });
+////        }
+//
+//
+//
+//
+//        },
+
         toggle: function(route /* this is location.hash */ ) {
             var sections = document.querySelectorAll('main section');
-            console.log(sections);
+//            console.log(sections);
 
             for (var i = 0; i < sections.length; i++) {
                 var sectionList = sections[i];
@@ -114,11 +160,11 @@
 
                 if (sectionsId === route) {
                     sectionList.classList.remove("hide");
-                    console.log(sectionsId);
-                    console.log(route);
+//                    console.log(sectionsId);
+//                    console.log(route);
 
                 } else if (route.length > 5) {
-                    console.log("true")
+//                    console.log("true")
                     sectionList.classList.add("hide"); config.detailSection.classList.remove('hide');
                 } else {
                     sectionList.classList.add("hide");
